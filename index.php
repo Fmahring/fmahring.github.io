f845129f9c6f919914f67c0d974a11e117e7121f
Fynn Mahrigner <fmahring.prof@gmail.com>
2024-03-22 09:56:23 +0100

    index commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+global $conn;
+include "PHP/utils/config.php";
+
+$result = $conn->query("SELECT * FROM demo");
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Vname: " . $row["vname"]. "   - NName: " . $row["nname"]. "   Zahl: " . $row["zahl"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
